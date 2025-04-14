fe0e3673958a76312b24c30c393047a73893ec71
zhangyixin <yasin66@163.com>
2025-04-14 15:15:38 +0800

    fix controller group error

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the jmx.f44f.com v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=jmx.f44f.com.jmx.f44f.com
+// +kubebuilder:object:generate=true
+// +groupName=jmx.f44f.com
 package v1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "jmx.f44f.com.jmx.f44f.com", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "jmx.f44f.com", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
